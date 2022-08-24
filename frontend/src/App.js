976f4ea024615d1019243940226adfa04a436afc
Siddarath-Kumar <83688562+Siddarath-Kumar@users.noreply.github.com>
2022-08-24 12:35:51 +1200

    Updated App.js, index.css and index.js files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,22 @@ import React from "react";
 import {
   Route,
   NavLink,
-  BrowserRouter as Router
+  BrowserRouter as Router,
+  Redirect
 } from "react-router-dom";
+
 import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
+import NotFoundPage from "./pages/404";
 
-//import NotFoundPage from "./pages/404";
 const App = () =>  {
     return (
         <Router>
         <div>
           <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
-                <li><NavLink to = "/">Home</NavLink></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
                 <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
@@ -23,6 +25,8 @@ const App = () =>  {
             <Route exact path="/" component={Home}/>
             <Route  path="/SEPractice" component={SEPractice}/>
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
+            <Route exact path="/404" component={NotFoundPage}/>
+            <Redirect to="/404"/>
           </div>
         </div>
         </Router>
